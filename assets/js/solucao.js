106180bee884c8c3dd9958ee5bf11a4e67648b7a
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-06-04 23:13:04 -0300

    refatoracao feita no js da pagina solucao

--- assets/js/solucao.js
+++ assets/js/solucao.js
@@ -1,4 +1,11 @@
 function solucaoCliente() {
-  alert("Muito obrigado por compartilhar sua solução conosco!");
+  input = document.getElementById("solucao-cliente").value;
+
+  if (input === "") {
+    alert("Por favor para enviar você precisa escrever sua solução!");
+  } else {
+    alert("Muito obrigado por compartilhar sua solução conosco!");
+  }
+
   document.getElementById("solucao-cliente").value = "";
 }
